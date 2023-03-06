2ae3cc70f0f1480af9021003b53506e7973f21ca
shylin04 <123716647+shylin04@users.noreply.github.com>
2023-03-06 19:11:19 +0530

    Add files via upload

---
+++ js task-13/script.js
@@ -0,0 +1,12 @@
+// store the color radio buttons in radios 
+const radios = document.getElementsByName('color');
+
+// box element
+const box = document.getElementById('box');
+
+// Add event listener to each radio button
+radios.forEach(radio => {
+  radio.addEventListener('click', () => {
+    box.style.backgroundColor = radio.value;
+  });
+});
