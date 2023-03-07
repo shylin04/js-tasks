5d7c31c39b025980adc0d5b57ac223ac8cc6b4db
M.Shylin Rexia <2000shylin@gmail.com>
2023-03-07 13:15:22 +0530

    commit changes

--- js task-1/script.js
+++ js task-1/script.js
@@ -1,11 +1,11 @@
-let previousClicked =[]; // define a previousClicked  to add the clicked button//
+let previousClicked =[]; // define a previousClicked  to add the clicked buttons//
  
 document.querySelectorAll('.box').forEach(function (box){
   box.addEventListener('click', function(){
  
     // if there is no previously clicked button//  
     if(previousClicked.length===0){ 
-        alert(this.innerHTML); // give alert messege of currently clicked button content  //
+        alert(this.innerHTML); // give alert messege for currently clicked button content  //
          previousClicked.push(this.innerHTML); // push the clicked button to the previousClicked //
      
         // if there is previously clicked button alert previosly clicked button and currently clicked button content//
